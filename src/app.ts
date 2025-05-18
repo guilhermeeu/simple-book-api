978d13845da273597f9941b2672a0a0563c6231b
kronos <guilhermegomes7770@gmail.com>
2025-05-18 02:47:01 -0300

    Third commit

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 // * Configs
-
 import express from "express";
 import type { Express } from "express";
 const app: Express = express();
 app.use(express.json());
+
 // * ROUTES
 import bookRoutes from "./routes/book.routes";
 app.use("/book", bookRoutes);
